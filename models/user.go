4fcb5955deab80ed9ad557fe497f6540e61d2d58
ccontreraso <ccontreras@geekz.cl>
2021-08-22 18:39:31 -0400

    Update user.go

--- models/user.go
+++ models/user.go
@@ -8,13 +8,13 @@ import (
 type User struct {
 	ID         primitive.ObjectID `bson:"_id, omitempty" json:"id"`
 	Name       string             `bson:"name" json:"name, omitempty"`
-	Surname    string             `bson:"surname" json:"name, omitempty"`
+	Surname    string             `bson:"surname" json:"surname, omitempty"`
 	Birthday   time.Time          `bson:"birthday" json:"birthday, omitempty"`
 	Email      string             `bson:"email" json:"email"`
-	Password   string             `bson:"name" json:"name, omitempty"`
-	Avatar     string             `bson:"name" json:"name, omitempty"`
-	Banner     string             `bson:"name" json:"name, omitempty"`
-	Biographic string             `bson:"name" json:"name, omitempty"`
-	Location   string             `bson:"location" json:"name, omitempty"`
-	Website    string             `bson:"website" json:"name, omitempty"`
+	Password   string             `bson:"password" json:"password, omitempty"`
+	Avatar     string             `bson:"avatar" json:"avatar, omitempty"`
+	Banner     string             `bson:"banner" json:"banner, omitempty"`
+	Biographic string             `bson:"biographic" json:"biographic, omitempty"`
+	Location   string             `bson:"location" json:"location, omitempty"`
+	Website    string             `bson:"website" json:"website, omitempty"`
 }
